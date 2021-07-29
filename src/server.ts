d6503bdb92ef33602f403cb02056ea759906ef21
Pedro Henrique <phcatanduba@gmail.com>
2021-07-28 22:40:25 -0300

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app, { init } from './app';
 
 init().then(() => {
-    app.listen(process.env.PORT || 4000, () => {
-        console.log(`Server is listening on port 4000.`);
+    const PORT = parseInt(process.env.PORT) || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}.`);
     });
 });
 
