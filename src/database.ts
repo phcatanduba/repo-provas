9207d11fa77cf03ed9bc6ac8e6f2b8de5c4443c9
Pedro Henrique <phcatanduba@gmail.com>
2021-07-28 21:17:07 -0300

    deploy test

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,18 @@
 import { getConnectionManager } from 'typeorm';
 import dotenv from 'dotenv';
 
-dotenv.config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
-});
-
-const { DB_URL } = process.env;
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+    dotenv.config({
+        path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+    });
+}
 
 export default async function connectDatabase() {
     const connectionManager = await getConnectionManager();
     const connection = connectionManager.create({
         name: 'default',
         type: 'postgres',
-        url: DB_URL || process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL,
         entities: ['src/entities/*.ts'],
     });
     await connection.connect();
