281ee8fc3219de28d0cdb39677259b153b3bc6ba
Pedro Henrique <phcatanduba@gmail.com>
2021-07-28 21:36:01 -0300

    testing deploy

--- src/database.ts
+++ src/database.ts
@@ -14,7 +14,6 @@ export default async function connectDatabase() {
         type: 'postgres',
         url: process.env.DATABASE_URL,
         entities: ['src/entities/*.ts'],
-        ssl: false,
     });
     await connection.connect();
 }
